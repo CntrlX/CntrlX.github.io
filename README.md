5b8e3baff63c4d7a95c69fad3f517efc62766576
Nihal anil <43931373+CntrlX@users.noreply.github.com>
2023-05-01 19:54:37 +0530

    change

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+# UecTechfestWebsite
--- src/UI/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import classes from "./Backdrop.module.css"
-
-const Backdrop = (props) => {
-  return (
-    <div className={classes.backdrop}>{props.children}</div>
-  )
-}
-
-export default Backdrop
